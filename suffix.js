066e4bbf66b1244c1c90e907d66f866c15c9e952
Mustafa Paksoy <paksoy@gmail.com>
2012-07-22 00:20:28 -0700

    Initial implementation

--- suffix.js
+++ suffix.js
@@ -1,7 +1,52 @@
 (function($, _) {
     "use strict";
+
+    var Node = (function() {
+        function Node() {
+            this.edges = {};
+        }
+
+        Node.prototype.addStr = function(str) {
+            if (! str) {
+                return;
+            }
+
+            var cur = str[0];
+            if (! this.edges[cur]) {
+                this.edges[cur] = new Node();
+            }
+
+            this.edges[cur].addStr(str.slice(1));
+        };
+
+        Node.prototype.toString = function() {
+            var ret = [];
+
+            _.each(this.edges, function(v, k) {
+                ret.push('[' + k + ',' + v + ']');
+            });
+
+            return ret.join('');
+        };
+
+        return Node;
+    })();
+
+
     function gen_suffix_tree(str) {
-        return str;
+        if (! str) {
+            return null;
+        }
+
+        var ret = new Node();
+        var substr = '';
+        for (var i = (str.length - 1); i >= 0; i--) {
+            substr = str[i] + substr;
+            ret.addStr(substr);
+        }
+
+        console.log('tree', ret);
+        return ret;
     }
 
     function update() {
@@ -14,4 +59,4 @@
     });
 
     $(update);
-})(jQuery, _)
+})(jQuery, _);
