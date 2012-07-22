dd6ac33ae9a4e98b56929a0c32998023f28044b0
Mustafa Paksoy <paksoy@gmail.com>
2012-07-21 23:45:22 -0700

    html scaffolding

--- suffix.js
+++ suffix.js
@@ -1,3 +1,17 @@
 (function($, _) {
     "use strict";
+    function gen_suffix_tree(str) {
+        return str;
+    }
+
+    function update() {
+        $('#output').text(gen_suffix_tree($('#input').val()));
+    }
+
+    $('form').submit(function() {
+        update();
+        return false;
+    });
+
+    $(update);
 })(jQuery, _)
